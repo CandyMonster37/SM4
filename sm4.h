ba98327be961a70ebcae8ef0a6047dd71f323603
X <76767579+CandyMonster37@users.noreply.github.com>
2022-11-04 21:44:02 +0800

    修改一处函数声明

--- sm4.h
+++ sm4.h
@@ -10,16 +10,16 @@
 #define ENCRYPT 0               // 加密模式
 #define DECRYPT 1               // 解密模式
 
-void int_to_four_char(u32 input32, u8 *output8);                                        // 1个u32拆成4个u8
-void four_char_to_int(u8 *input8, u32 *output32);                                       // 4个u8合成1个u32
-u32 round_rotation_left(u32 input, int len);                                            // 循环左移，同汇编ROL
+void int_to_four_char(u32 input32, u8 *output8);                                            // 1个u32拆成4个u8
+void four_char_to_int(u8 *input8, u32 *output32);                                           // 4个u8合成1个u32
+u32 round_rotation_left(u32 input, int len);                                                // 循环左移，同汇编ROL
 
-void gen_round_keys(u8 *usr_key, u32 *rk_array);                                        // 轮密钥生成函数
-void reverse_change_R(u32 *dst, u32 *src);                                              // 反序变换R
-u32 round_fun_F(u32 *input128, u32 rk);                                                 // 轮函数F
-u32 syn_trans_T(u32 input);                                                             // 合成置换T
-void nl_tor(u8 *input_tor, u8 *output_tor);                                             // 非线性变换TOR (S_BOX)
-void crypt_128bit(u32 *usr_data, u32 *usr_key, u32 *crypted_data, int crypt_mode);      // 加密/解密公用的函数体
+void gen_round_keys(u8 *usr_key, u32 *rk_array);                                            // 轮密钥生成函数
+void reverse_change_R(u32 *dst, u32 *src);                                                  // 反序变换R
+u32 round_fun_F(u32 *input128, u32 rk);                                                     // 轮函数F
+u32 syn_trans_T(u32 input);                                                                 // 合成置换T
+void nl_tor(u8 *input_tor, u8 *output_tor);                                                 // 非线性变换TOR (S_BOX)
+void crypt_128bit_ECB(u32 *usr_data, u32 *rk_array, u32 *crypted_data, int crypt_mode);     // ECB模式加密/解共用的函数体
 
 /* **************************************  以下为固定参数  ************************************** */
 
